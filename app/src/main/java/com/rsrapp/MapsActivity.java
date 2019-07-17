eba1bc9cb7e72f45afc56e8612ca1cb0b234070f
Roland Butacu <1541224@brunel.ac.uk>
2019-07-17 17:48:54 +0100

    Map now gives device address in infoWindow

--- app/src/main/java/com/rsrapp/MapsActivity.java
+++ app/src/main/java/com/rsrapp/MapsActivity.java
@@ -9,10 +9,13 @@ import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -32,6 +35,10 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements
         OnMapReadyCallback,
@@ -71,7 +78,7 @@ public class MapsActivity extends FragmentActivity implements
 
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             buildGoogleApiClient();
-            mMap.setMyLocationEnabled(true);
+           // mMap.setMyLocationEnabled(true); //This enables the Current Device location dot, including orientation
         }
 
 
@@ -207,13 +214,39 @@ public class MapsActivity extends FragmentActivity implements
 
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
 
+        //This animates the camera with movement and zooming towards device location
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 16);
+        mMap.animateCamera(cameraUpdate);
+
+
         mMap.setInfoWindowAdapter(new CustomInfoWindowAdapter(MapsActivity.this));
+
+
+      // String myAddress;
+      //  myAddress = getAddress(location);
+
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        List<Address> addresses  = null;
+        try {
+            addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(), 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String address = addresses.get(0).getAddressLine(0);
+        /**
+        String city = addresses.get(0).getLocality();
+        String state = addresses.get(0).getAdminArea();
+        String zip = addresses.get(0).getPostalCode();
+        String country = addresses.get(0).getCountryName();
+        */
 
 
         final MarkerOptions markerOptions = new MarkerOptions();
        markerOptions.position(latLng);
-       markerOptions.title("Uw locatie");
-       markerOptions.snippet("70 Mayhill Road, EN5 2NP, LulCity");
+       markerOptions.snippet(address);
        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.map_marker));
         //markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
 
@@ -225,9 +258,7 @@ public class MapsActivity extends FragmentActivity implements
         currentUserLocationMarker.showInfoWindow();
 
 
-        //This animates the camera with movement and zooming towards device location
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 16);
-        mMap.animateCamera(cameraUpdate);
+
 
 
 
@@ -265,6 +296,16 @@ public class MapsActivity extends FragmentActivity implements
 
     }
 
+    public String getAddress(Location location)
+    {
+
+
+
+
+
+
+      return null;
+    }
 
 
     @Override
